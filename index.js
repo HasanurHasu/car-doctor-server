3885d883e26d26756b7ccc9765e565c34e353078
Md. Hasanur Rahman <hasanurhasu127@gmail.com>
2023-10-30 15:55:24 +0600

    added booking and services

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7dcoggr.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,11 +25,46 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const servicesCollections = client.db('carDoctor').collection('services');
+        const bookingsCollections = client.db('carDoctor').collection('booking')
+
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollections.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const options = {
+                projection: { _id: 1, title: 1, price: 1, img: 1 },
+            };
+            const result = await servicesCollections.findOne(query, options);
+            res.send(result)
+        })
+
+        // bookings
+
+        app.get('/bookings', async(req, res) => {
+            const cursor = await bookingsCollections.find().toArray();
+            res.send(cursor);
+        })
+
+        app.post('/bookings', async(req, res) => {
+            const booking = req.body;
+            const result = await bookingsCollections.insertOne(booking);
+            res.send(result);
+
+        } )
+
+        
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
-        
+
 
     } finally {
         // Ensures that the client will close when you finish/error
